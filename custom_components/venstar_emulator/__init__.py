4ab091943f2bc136e7fa85722d42c4b6979e085a
CaliusOptimus <caliusoptimus@gmail.com>
2026-02-28 11:59:47 -0500

    Initial Tweaks

--- custom_components/venstar_emulator/__init__.py
+++ custom_components/venstar_emulator/__init__.py
@@ -5,11 +5,14 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.storage import Store
 
 from .const import DOMAIN, IntegrationData, PLATFORMS, STORAGE_VERSION
 from .coordinator import VenstarCoordinator
 from .runtime import VenstarRuntime
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
